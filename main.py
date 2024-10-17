2a16673ebcdb8fc27332f52a5e5655131521686b
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-10-16 21:47:36 -0700

    update main.py w/ custom formatter
    So I can combine default values as well as formatting the text for the epilog w/ no issues

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ Also to be use a skeleton for a later React to do list
 """
 import argparse # for handling flags
 import sqlite3 # for db
+import textwrap # for wrapping epilog
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    """Custom formatter that combines default values and raw description formatting."""
+    pass
+parser = argparse.ArgumentParser(
+                    prog='To do list',
+                    formatter_class=CustomFormatter,
+                    description='A simple to do list cli app',
+                    epilog=textwrap.dedent('''\
+                    Thank you for using!
+                    Check out more projects at:
+                    https://github.com/JevonThompsonx?tab=repositories
+                    '''),
+                    )
+parser.print_help()
