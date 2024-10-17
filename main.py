dfc4976f397521678e261ef399952c9b6933502c
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-10-16 19:50:57 -0700

    Base for arg and sqlite3 imports
    Added the 2 modules I know I'll be using
    
    I got the basics from chatgpt already but I'm going to visit the documentation of argparse before I use it then continue

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+A CLI to do list application
+For practicing using python flags, sqlite3 for the database
+Also to be use a skeleton for a later React to do list
+    w/ bunsqlite, typescript and hono backend 
+"""
+import argparse # for handling flags
+import sqlite3 # for db
